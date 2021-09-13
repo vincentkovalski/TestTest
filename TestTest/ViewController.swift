ec40add3d8db2d38f83c31e1ee22bb3d9528c07a
Виктор Ковалевский <kovall89@gmail.com>
2021-09-13 22:50:06 +0300

    Add new func

--- TestTest/ViewController.swift
+++ TestTest/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    func someMethod() {
-
+    func setupView() {
+        view.backgroundColor = .systemYellow
     }
 
 }
