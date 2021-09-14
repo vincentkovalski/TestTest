522bff3de9a2b43be96858ffd94184ca419c6a0c
Виктор Ковалевский <kovall89@gmail.com>
2021-09-14 09:03:53 +0300

    create fierst view

---
+++ TestTest/firstviewcont.swift
@@ -0,0 +1,14 @@
+//
+//  firstviewcont.swift
+//  TestTest
+//
+//  Created by Ковалевский on 14.09.2021.
+//
+
+import UIKit
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
