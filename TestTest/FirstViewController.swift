988d8821c23e927a68b541de8c29d9273108907a
Виктор Ковалевский <kovall89@gmail.com>
2021-09-14 22:13:10 +0300

    Create First

---
+++ TestTest/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  TestTest
+//
+//  Created by Ковалевский on 14.09.2021.
+//
+
+import Foundation
+import UIKit
+
+final class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let subview = UIView()
+        view.addSubview(subview)
+    }
+}
